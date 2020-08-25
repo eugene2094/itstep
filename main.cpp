d3fc1e35b33c09ebd42514fb0f1779b06436a3fa
eugene2094 <70202369+eugene2094@users.noreply.github.com>
2020-08-25 20:13:19 +0300

    changed main.cpp

--- main.cpp
+++ main.cpp
@@ -3,3 +3,13 @@ void main()
 	cout << "hello" << endl;
         cout << "Buy" << endl;
 }
+void main2()
+{
+	cout << "hello" << endl;
+        cout << "Buy" << endl;
+}
+void main3()
+{
+	cout << "hello" << endl;
+        cout << "Buy" << endl;
+}
